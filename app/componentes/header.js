1d813da5b3a7ab79dfa77e6a1034623b42761e6c
Renato França <101846374+SirReinato@users.noreply.github.com>
2023-07-07 21:48:34 -0300

    projeto já completo
    Esse foi após um dia de serviço, conseguir finalizar as 21:47, agora partir para a segunda fase

---
+++ app/componentes/header.js
@@ -0,0 +1,9 @@
+const btnHamburguer = document.querySelector(".btn-hamburguer");
+
+btnHamburguer.addEventListener("click", menuHamburguer);
+
+function menuHamburguer() {
+  const menu = document.querySelector(".menu-hamburguer");
+
+  menu.classList.toggle("ativo");
+}
