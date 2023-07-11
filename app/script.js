0e9d260fb66bcd9011254d592011716c4d244a85
Renato França <renatofranca2881@gmail.com>
2023-07-10 22:29:05 -0300

    começo do modal

--- app/script.js
+++ app/script.js
@@ -12,3 +12,36 @@ const swiper = new Swiper(".swiper", {
 
 // Now you can use all slider methods like
 swiper.slideNext();
+
+const btn = document.querySelector(".menu__button");
+console.log(btn);
+btn.addEventListener("click", funcaoMenuHamburgue);
+
+function funcaoMenuHamburgue(event) {
+  event.preventDefault()
+  const menuNav = document.querySelector(".menu__nav");
+  menuNav.classList.toggle("menu-active");
+
+  btn.textContent === "☰"
+    ? (btn.textContent = "☓")
+    : (btn.textContent = "☰");
+}
+
+
+// MODAL ______________________________________
+// A modal em si
+const modal = document.querySelector(".modal");
+const abriModealBtn = document.querySelector(".open");
+const fecharModalBtn = document.querySelector(".close");
+
+/* Ao clicar no botão de Abrir Modal, executamos a função
+showModal() na Modal: */
+
+// setTimeout(() =>{
+//   modal.showModal();
+// }, '3000');
+
+abriModealBtn.addEventListener("click", () => modal.showModal());
+/* Ao clicar no botão de Fechar Modal, executamos a função
+close() na Modal */
+fecharModalBtn.addEventListener("click", () => modal.close());
